548ccf0861393e52b0b2f1bcf942d683b45880c4
ooSantazaoo <73683767+ooSantazaoo@users.noreply.github.com>
2020-10-30 16:38:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     </head>
     <body>
         <h1>SANTA NAKANO</h1>
-        <p>นายซันตะ นาคาโน</p>
+        <p>นายซันตะ นาคาโน ชื่อเล่น ซัน</p>
         <img src="cat2.jpg">
         <img src="cat5.jpg">
         <br>
@@ -13,11 +13,12 @@
         <a href="http://www.google.com">google link</a><br>
         <a href="http://www.facebook.com">facebook link</a><br>
 
-        <h2>เรื่องตลก</h2>
+        <h2>เรื่องตลก เรื่อง อยู่ได้อีก 3 วัน</h2>
         <ul>
-            <li>Google</li>
-            <li>Yahoo</li>
-            <li>Amazon</li>
+            <p>หมอคุยกับคนไข้</p>
+            <p>หมอ : คุณอยู่ รพ.ได้อีกแค่ 3 วันนะครับ</p>
+            <p>คนไข้ : ห๊าาา…ผมอาการหนักขนาดนั้นเลยเหรอครับ?</p>
+            <p>หมอ : ก็ไม่หนักหรอก….วันที่ 4 กลับบ้านได้</p>
         </ul>
     </body>
 </html>
