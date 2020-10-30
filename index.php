a551b0878490f78d6e7cc253ce8b6b6fc982b654
ooSantazaoo <73683767+ooSantazaoo@users.noreply.github.com>
2020-10-30 16:24:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <h1>SANTA NAKANO</h1>
         <p>นายซันตะ นาคาโน</p>
         <img src="cat2.jpg">
-        <img src="cat.jpeg">
+        <img src="cat5.jpg">
         <br>
         <a href="http://www.it.kmitl.ac.th">KMITL IT link</a>
         <a href="http://www.google.com">google link</a>
